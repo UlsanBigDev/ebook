09cbd2ba433c6cf3e9172ef07c4c589cde267843
CaKlan <lhs74187@gmail.com>
2022-11-19 20:27:59 +0900

    사운드 설정 버튼

---
+++ eBook/Assets/script/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void SelectBook(int bookId)
+    {
+        Debug.Log("당신이 선택한 책의 아이디 : " + bookId);
+        SceneManager.LoadScene("Book"+bookId);
+    }
+
+    public void BookEscape()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
---
+++ eBook/Assets/script/book/Book.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour, IBook
+{
+    public string subject;
+    public GameObject[] storys;
+    public GameObject nowStory;
+    public int page;
+
+    void Awake()
+    {
+        this.page = 0;
+        Transform storyList = this.transform.GetChild(0);
+        this.storys = new GameObject[storyList.childCount];
+        for (int i = 0; i < storyList.childCount; i++) {
+            GameObject go = storyList.GetChild(i).gameObject;
+            this.storys[i] = go;
+        }
+        // this.storys[0].SetActive(true);
+        SetPage(this.page);
+        
+    }
+    void ActiveStory(GameObject story) {
+        Debug.Log(story);
+        if (nowStory != null)
+        {
+            nowStory.SetActive(false);
+        }
+        nowStory = story;
+        nowStory.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { // 좌클릭
+            if (this.page < storys.Length-1) 
+            {
+                SetPage(this.page + 1);
+            }            
+        }
+
+        if (Input.GetMouseButtonDown(1)) // 우클릭
+        {
+            if (this.page > 0)
+            {
+                SetPage(this.page - 1);
+            }
+        }
+
+    }
+
+    public void SetPage(int page) {
+        this.page = page;
+        ActiveStory(this.storys[this.page]);
+        OnPageChange(this.page);
+    }
+
+    public void OnPageChange(int page)
+    {
+        AudioSource audio = nowStory.GetComponent<AudioSource>();
+        audio.Play();
+        Debug.Log("페이지 체인지 이벤트");
+    }
+}
